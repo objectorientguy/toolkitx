63414445f9f26282aa9d7316fef7404542061373
Aditya <88383068+objectorientguy@users.noreply.github.com>
2023-05-19 16:35:45 +0530

    TXAD-57: Implement on tap method for navigation

--- lib/screens/home/widgets/online_modules.dart
+++ lib/screens/home/widgets/online_modules.dart
@@ -20,35 +20,45 @@ class OnLineModules extends StatelessWidget {
             crossAxisSpacing: midTinySpacing,
             mainAxisSpacing: midTinySpacing),
         itemBuilder: (BuildContext context, int index) {
-          return Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(kCardRadius),
-            ),
-            color: AppColor.lightestBlue,
-            shadowColor: AppColor.ghostWhite,
-            elevation: kCardElevation,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  child: Image.asset(
-                      ModulesUtil.listModulesMode[index].moduleImage,
-                      height: kModuleIconSize,
-                      width: kModuleIconSize),
-                ),
-                const SizedBox(height: tinySpacing),
-                Padding(
-                  padding: const EdgeInsets.only(
-                      left: tiniestSpacing, right: tiniestSpacing),
-                  child: Text(
-                    ModulesUtil.listModulesMode[index].moduleName,
-                    textAlign: TextAlign.center,
+          return InkWell(
+            onTap: () => navigateToModule(index),
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(kCardRadius),
+              ),
+              color: AppColor.lightestBlue,
+              shadowColor: AppColor.ghostWhite,
+              elevation: kCardElevation,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: Image.asset(
+                        ModulesUtil.listModulesMode[index].moduleImage,
+                        height: kModuleIconSize,
+                        width: kModuleIconSize),
                   ),
-                ),
-              ],
+                  const SizedBox(height: tinySpacing),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: tiniestSpacing, right: tiniestSpacing),
+                    child: Text(
+                      ModulesUtil.listModulesMode[index].moduleName,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         });
+  }
+
+  navigateToModule(index) {
+    switch (index) {
+      case 0:
+        break;
+    }
   }
 }
