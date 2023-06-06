1a0a243abe9965ca5a61bcbdae3f1bc3396a62be
Aditya Rana <88383068+objectorientguy@users.noreply.github.com>
2023-06-06 21:24:46 +0530

    TXAD-24: Implement permit list TAB 5 (#28)
    * TXAD-21: Implement permit list TAB 2
    
    * TXAD-21: Implement permit list TAB 3
    
    * TXAD-22: Implement permit list TAB 3
    
    * TXAD-22: Implement permit list TAB 3
    
    * TXAD-23: Implement permit list TAB 4
    
    * TXAD-24: Implement permit list TAB 5

--- lib/screens/permit/widgets/permit_additional_info.dart
+++ lib/screens/permit/widgets/permit_additional_info.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toolkit/configs/app_color.dart';
 import 'package:toolkit/configs/app_theme.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import '../../../configs/app_spacing.dart';
 import '../../../data/models/permit/permit_details_model.dart';
 
@@ -12,85 +13,117 @@ class PermitAdditionalInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: tiny),
-        Text(
-          'Additional Information',
-          style: Theme.of(context)
-              .textTheme
-              .medium
-              .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: xxTinierSpacing),
-        const SizedBox(height: tiny),
-        Text(
-          'Method Statement',
-          style: Theme.of(context)
-              .textTheme
-              .medium
-              .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: xxTinierSpacing),
-        Text(permitDetailsModel.data!.tab2!.methodStatement!,
-            style: Theme.of(context).textTheme.small),
-        const SizedBox(height: tiny),
-        Text(
-          'Relevant Info',
-          style: Theme.of(context)
-              .textTheme
-              .medium
-              .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: xxTinierSpacing),
-        Text(permitDetailsModel.data!.tab2!.generalMessage,
-            style: Theme.of(context).textTheme.small),
-        const SizedBox(height: tiny),
-        Text(
-          'Special Work',
-          style: Theme.of(context)
-              .textTheme
-              .medium
-              .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: xxTinierSpacing),
-        Text(permitDetailsModel.data!.tab2!.specialWork!,
-            style: Theme.of(context).textTheme.small),
-        const SizedBox(height: tiny),
-        Text(
-          'Specific PPR',
-          style: Theme.of(context)
-              .textTheme
-              .medium
-              .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: xxTinierSpacing),
-        Text(permitDetailsModel.data!.tab2!.specialppe!,
-            style: Theme.of(context).textTheme.small),
-        const SizedBox(height: tiny),
-        Text(
-          'Protective Measures',
-          style: Theme.of(context)
-              .textTheme
-              .medium
-              .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: xxTinierSpacing),
-        Text(permitDetailsModel.data!.tab2!.protectivemeasures!,
-            style: Theme.of(context).textTheme.small),
-        const SizedBox(height: tiny),
-        Text(
-          'Layout',
-          style: Theme.of(context)
-              .textTheme
-              .medium
-              .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(height: xxTinierSpacing),
-        Text(permitDetailsModel.data!.tab2!.layout!,
-            style: Theme.of(context).textTheme.small),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: tiny),
+          Text(
+            'Additional Information',
+            style: Theme.of(context)
+                .textTheme
+                .medium
+                .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: xxTinierSpacing),
+          const SizedBox(height: tiny),
+          Text(
+            'Method Statement',
+            style: Theme.of(context)
+                .textTheme
+                .medium
+                .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: xxTinierSpacing),
+          Text(permitDetailsModel.data.tab2.methodStatement,
+              style: Theme.of(context).textTheme.small),
+          const SizedBox(height: tiny),
+          Text(
+            'Relevant Info',
+            style: Theme.of(context)
+                .textTheme
+                .medium
+                .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: xxTinierSpacing),
+          Text(permitDetailsModel.data.tab2.generalMessage,
+              style: Theme.of(context).textTheme.small),
+          const SizedBox(height: tiny),
+          Text(
+            'Special Work',
+            style: Theme.of(context)
+                .textTheme
+                .medium
+                .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: xxTinierSpacing),
+          Text(permitDetailsModel.data.tab2.specialWork,
+              style: Theme.of(context).textTheme.small),
+          const SizedBox(height: tiny),
+          Text(
+            'Specific PPR',
+            style: Theme.of(context)
+                .textTheme
+                .medium
+                .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: xxTinierSpacing),
+          Text(permitDetailsModel.data.tab2.specialppe,
+              style: Theme.of(context).textTheme.small),
+          const SizedBox(height: tiny),
+          Text(
+            'Protective Measures',
+            style: Theme.of(context)
+                .textTheme
+                .medium
+                .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: xxTinierSpacing),
+          Text(permitDetailsModel.data.tab2.protectivemeasures,
+              style: Theme.of(context).textTheme.small),
+          const SizedBox(height: tiny),
+          Text(
+            'Layout',
+            style: Theme.of(context)
+                .textTheme
+                .medium
+                .copyWith(color: AppColor.black, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: xxTinierSpacing),
+          InkWell(
+            onTap: () {
+              launchUrlString(permitDetailsModel.data.tab2.layoutLink,
+                  mode: LaunchMode.inAppWebView);
+            },
+            child: Text(permitDetailsModel.data.tab2.layout,
+                style: Theme.of(context)
+                    .textTheme
+                    .small
+                    .copyWith(color: AppColor.deepBlue)),
+          ),
+          const SizedBox(height: tiny),
+          ListView.builder(
+              shrinkWrap: true,
+              itemCount: permitDetailsModel.data.tab2.customfields.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(permitDetailsModel.data.tab2.customfields[index].title,
+                        style: Theme.of(context).textTheme.medium.copyWith(
+                            color: AppColor.black,
+                            fontWeight: FontWeight.bold)),
+                    const SizedBox(height: xxTinierSpacing),
+                    Text(
+                        permitDetailsModel
+                            .data.tab2.customfields[index].fieldvalue,
+                        style: Theme.of(context).textTheme.small),
+                  ],
+                );
+              }),
+        ],
+      ),
     );
   }
 }
