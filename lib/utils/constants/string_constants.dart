e179f81f1bf4040768ed6da020bbbe919adabcec
Aditya <88383068+objectorientguy@users.noreply.github.com>
2023-06-15 19:20:09 +0530

    TXAD-19: Implement permit filter screen

--- lib/utils/constants/string_constants.dart
+++ lib/utils/constants/string_constants.dart
@@ -1,56 +1,33 @@
 abstract class StringConstants {
-  static const String kToolKitX = 'ToolKit X';
-  static const String kAppIntroduction =
-      'Workforce management designed \n to enhance efficiency, productivity \n and safety';
   static const String kAppIntroductionNoSpacing =
       'Workforce management designed to enhance efficiency, productivity and safety';
   static const String kWelcomeToToolkitX = 'Welcome to ToolKitX';
   static const String kStartNow = 'START NOW';
   static const String kSelectTimeZone = 'Select Your TimeZone';
-  static const String kEmailAddress = 'Email Address';
   static const String kPassword = 'Password'; //
-  static const String kNext = 'NEXT';
-  static const String kLogin = 'LOGIN';
-  static const String kWelcome = 'Welcome';
-  static const String kType = 'Type';
-  static const String kGenerateOtp = 'GENERATE OTP';
   static const String kEnterNewPassword =
       'Enter New Password'; // comment lines indicates that keys are not available
   static const String kConfirmPassword = 'Confirm Password'; //
   static const String kOtpGenerated = 'OTP generated';
   static const String kSelectDateFormat = 'Select Your Date Format';
-  static const String kSave = 'SAVE';
   static const String kOldPassword = 'Old Password'; //
   static const String kEnterOtp = 'Enter OTP'; //
   static const String kSelectYourLanguage = 'Select Your Language';
-  static const String kPleaseConfirm = 'Please Confirm';
   static const String kReject = 'Reject'; //
   static const String kDialogContent =
       "The language file doesn't exist or an old version.Please download the latest language file."; //
-  static const String kNo = 'NO';
-  static const String kYes = 'YES';
   static const String kReload = 'RELOAD'; //
   static const String kOk = 'Ok';
   static const String kDateFormatValidate = 'Please select a Date Format!'; //
-  static const String kEditProfile = 'Edit Profile';
-  static const String kChangeClient = 'Change Client';
-  static const String kLogout = 'Logout';
   static const String kChangePassword = 'Change Password'; //
   static const String kChangeLanguage = 'Change Language';
   static const String kChangeTimezone = 'Change Timezone';
   static const String kChangeDateFormat = 'Change DateFormat';
-  static const String kToolKitEmail = 'Mail us at info@toolkit.com';
-  static const String kPrivacyPolicy = 'Privacy Policy';
-  static const String kMaturityModel = 'PTW Maturity Model';
   static const String kPrivacyPolicyUrl =
       'https://www.toolkitx.com/privacypolicy.html';
   static const String kMaturityModelUrl =
       'https://www.toolkitx.com/campaign/permit-to-work/';
   static const String kToolkitXEmail = 'info@toolkit.com';
-  static const String kFirstName = 'First Name';
-  static const String kLastName = 'Last Name';
-  static const String kContact = 'Contact';
-  static const String kBloodGroup = 'Blood Group';
   static const String kSelectBloodGroup = 'Select Blood Group'; //
   static const String kSelectCategory = 'Select Category';
   static const String kSignature = 'Signature';
@@ -112,8 +89,8 @@ abstract class StringConstants {
   static const String kValidateConfirmPassword =
       'Please enter confirm password';
   static const String kValidatePasswordMatch = 'Passwords must match!';
-      'We could not load the information. Please\ncheck the internet connection and refresh\nthis screen so we can synchronize the data.';
   static const String kPermitType = 'Permit Type';
   static const String kEmergency = 'Emergency';
   static const String kKeywords = 'Keywords';
+  static const String kFilter = 'Filter';
 }
