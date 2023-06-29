9e42500d0c5f922eb63d72b6400b5d06e74d9732
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-06-29 12:31:24 +0530

    TXAD-14: Add Floating Action Button to Injured Person List Tab.

--- lib/screens/incident/widgets/incident_injured_person_list.dart
+++ lib/screens/incident/widgets/incident_injured_person_list.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:toolkit/configs/app_theme.dart';
 import 'package:toolkit/data/models/incident/incident_details_model.dart';
+import 'package:toolkit/utils/database_utils.dart';
 import 'package:toolkit/utils/incident_util.dart';
 
 import '../../../configs/app_color.dart';
@@ -19,41 +20,55 @@ class IncidentInjuredPersonList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return (incidentDetailsModel.data!.injuredpersonlist!.isEmpty)
-        ? Center(
-            child: Text(StringConstants.kNoInjuredPerson,
-                style: Theme.of(context).textTheme.small.copyWith(
-                    fontWeight: FontWeight.w700, color: AppColor.mediumBlack)))
-        : ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            padding: EdgeInsets.zero,
-            shrinkWrap: true,
-            itemCount: incidentDetailsModel.data!.injuredpersonlist!.length,
-            itemBuilder: (context, index) {
-              final random = Random();
-              final leadingAvatarIcon = IncidentUtil().leadingAvatarList[
-                  random.nextInt(IncidentUtil().leadingAvatarList.length)];
-              return CustomCard(
-                child: ListTile(
-                  contentPadding: const EdgeInsets.only(
-                      left: tinier,
-                      right: tinier,
-                      top: tiniest,
-                      bottom: tiniest),
-                  trailing: Image.asset(
-                    leadingAvatarIcon,
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          label: Row(
+            children: [
+              const Icon(Icons.add),
+              const SizedBox(width: tiniest),
+              Text(DatabaseUtil.getText('addInjuredPersonPageHeading'))
+            ],
+          ),
+          onPressed: () {}),
+      body: (incidentDetailsModel.data!.injuredpersonlist!.isEmpty)
+          ? Center(
+              child: Text(StringConstants.kNoInjuredPerson,
+                  style: Theme.of(context).textTheme.small.copyWith(
+                      fontWeight: FontWeight.w700,
+                      color: AppColor.mediumBlack)))
+          : ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              itemCount: incidentDetailsModel.data!.injuredpersonlist!.length,
+              itemBuilder: (context, index) {
+                final random = Random();
+                final leadingAvatarIcon = IncidentUtil().leadingAvatarList[
+                    random.nextInt(IncidentUtil().leadingAvatarList.length)];
+                return CustomCard(
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.only(
+                        left: tinier,
+                        right: tinier,
+                        top: tiniest,
+                        bottom: tiniest),
+                    trailing: Image.asset(
+                      leadingAvatarIcon,
+                    ),
+                    title: Text(
+                        incidentDetailsModel
+                            .data!.injuredpersonlist![index].name,
+                        style: Theme.of(context).textTheme.small.copyWith(
+                            fontWeight: FontWeight.w700,
+                            color: AppColor.mediumBlack)),
                   ),
-                  title: Text(
-                      incidentDetailsModel.data!.injuredpersonlist![index].name,
-                      style: Theme.of(context).textTheme.small.copyWith(
-                          fontWeight: FontWeight.w700,
-                          color: AppColor.mediumBlack)),
-                ),
-              );
-            },
-            separatorBuilder: (context, index) {
-              return const SizedBox(height: xxTinierSpacing);
-            },
-          );
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(height: xxTinierSpacing);
+              },
+            ),
+    );
   }
 }
