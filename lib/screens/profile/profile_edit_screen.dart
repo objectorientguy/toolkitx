08f1f16229e31f770f3d55650dcc437354242d29
404ajayreddy <arg4644@gmail.com>
2023-07-04 12:47:22 +0530

    TXAD-106 Implemented Back Confirmation On Edit

--- lib/screens/profile/profile_edit_screen.dart
+++ lib/screens/profile/profile_edit_screen.dart
@@ -21,6 +21,7 @@ import 'widgets/signature.dart';
 
 class ProfileEditScreen extends StatelessWidget {
   static const routeName = 'ProfileEditScreen';
+  static Map copyProfileMap = {};
 
   const ProfileEditScreen({Key? key}) : super(key: key);
 
@@ -28,126 +29,130 @@ class ProfileEditScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<PickAndUploadImageBloc>().add(UploadInitial());
     context.read<ProfileBloc>().add(DecryptUserProfileData());
-    return Scaffold(
-        appBar: GenericAppBar(title: DatabaseUtil.getText('MyProfile')),
-        body: BlocConsumer<ProfileBloc, ProfileStates>(
-            buildWhen: (previousState, currentState) =>
-                currentState is EditProfileInitialized ||
-                currentState is EditProfileInitializing,
-            listener: (BuildContext context, state) {
-              if (state is UserProfileUpdating) {
-                ProgressBar.show(context);
-              }
-              if (state is UserProfileUpdated) {
-                ProgressBar.dismiss(context);
-                Navigator.pushNamed(context, RootScreen.routeName,
-                    arguments: false);
-              }
-              if (state is UserProfileUpdateError) {
-                ProgressBar.dismiss(context);
-                showCustomSnackBar(context, state.message, '');
-              }
-              if (state is UserProfileUpdateCancel) {
-                Navigator.pop(context);
-              }
-            },
-            builder: (context, state) {
-              if (state is EditProfileInitializing) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (state is EditProfileInitialized) {
-                return SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    child: Padding(
-                        padding: const EdgeInsets.only(
-                            left: leftRightMargin, right: leftRightMargin),
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const SizedBox(height: xxxSmallerSpacing),
-                              Text(DatabaseUtil.getText('FirstName'),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .xSmall
-                                      .copyWith(fontWeight: FontWeight.w600)),
-                              const SizedBox(height: xxxTinierSpacing),
-                              TextFieldWidget(
-                                  value: state.profileDetailsMap['fname'],
-                                  textInputAction: TextInputAction.next,
-                                  maxLength: 50,
-                                  hintText: DatabaseUtil.getText('FirstName'),
-                                  onTextFieldChanged: (String textField) {
-                                    state.profileDetailsMap['fname'] =
-                                        textField;
-                                  }),
-                              const SizedBox(height: xxTinySpacing),
-                              Text(DatabaseUtil.getText('LastName'),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .xSmall
-                                      .copyWith(fontWeight: FontWeight.w600)),
-                              const SizedBox(height: xxxTinierSpacing),
-                              TextFieldWidget(
-                                  value: state.profileDetailsMap['lname'],
-                                  maxLength: 50,
-                                  textInputAction: TextInputAction.next,
-                                  hintText: DatabaseUtil.getText('LastName'),
-                                  onTextFieldChanged: (String textField) {
-                                    state.profileDetailsMap['lname'] =
-                                        textField;
-                                  }),
-                              const SizedBox(height: xxTinySpacing),
-                              Text(DatabaseUtil.getText('Contact'),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .xSmall
-                                      .copyWith(fontWeight: FontWeight.w600)),
-                              const SizedBox(height: xxxTinierSpacing),
-                              TextFieldWidget(
-                                  value: state.profileDetailsMap['contact'],
-                                  textInputType: TextInputType.phone,
-                                  textInputAction: TextInputAction.done,
-                                  maxLength: 20,
-                                  hintText: DatabaseUtil.getText('Contact'),
-                                  onTextFieldChanged: (String textField) {
-                                    state.profileDetailsMap['contact'] =
-                                        textField;
-                                  }),
-                              const SizedBox(height: xxTinySpacing),
-                              Text(DatabaseUtil.getText('BloodGroup'),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .xSmall
-                                      .copyWith(fontWeight: FontWeight.w600)),
-                              const SizedBox(height: xxxTinierSpacing),
-                              BloodGroupExpansionTile(
-                                  profileDetailsMap: state.profileDetailsMap),
-                              const SizedBox(height: xxTinySpacing),
-                              SignaturePad(
-                                  map: state.profileDetailsMap, mapKey: 'sign'),
-                              const SizedBox(height: xxxSmallerSpacing),
-                              PrimaryButton(
-                                  onPressed: () {
-                                    context.read<ProfileBloc>().add(
-                                        UpdateProfile(
-                                            updateProfileMap:
-                                                state.profileDetailsMap));
-                                  },
-                                  textValue:
-                                      DatabaseUtil.getText('buttonSave')),
-                              const SizedBox(height: xxxSmallerSpacing)
-                            ])));
-              } else if (state is EditProfileError) {
-                return Center(
-                    child: GenericReloadButton(
-                        onPressed: () {
-                          context
-                              .read<ProfileBloc>()
-                              .add(DecryptUserProfileData());
-                        },
-                        textValue: StringConstants.kReload));
-              } else {
-                return const SizedBox();
-              }
-            }));
+    return WillPopScope(
+      onWillPop: () => DatabaseUtil.showExitPopup(
+          context.read<ProfileBloc>().updateProfileDataMap,
+          copyProfileMap,
+          context),
+      child: Scaffold(
+          appBar: GenericAppBar(title: DatabaseUtil.getText('MyProfile')),
+          body: BlocConsumer<ProfileBloc, ProfileStates>(
+              buildWhen: (previousState, currentState) =>
+                  currentState is EditProfileInitialized ||
+                  currentState is EditProfileInitializing,
+              listener: (BuildContext context, state) {
+                if (state is UserProfileUpdating) {
+                  ProgressBar.show(context);
+                }
+                if (state is UserProfileUpdated) {
+                  ProgressBar.dismiss(context);
+                  Navigator.pushNamed(context, RootScreen.routeName,
+                      arguments: false);
+                }
+                if (state is UserProfileUpdateError) {
+                  ProgressBar.dismiss(context);
+                  showCustomSnackBar(context, state.message, '');
+                }
+                if (state is UserProfileUpdateCancel) {
+                  Navigator.pop(context);
+                }
+              },
+              builder: (context, state) {
+                if (state is EditProfileInitializing) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is EditProfileInitialized) {
+                  copyProfileMap = Map.of(state.profileDetailsMap);
+                  return SingleChildScrollView(
+                      physics: const BouncingScrollPhysics(),
+                      child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: leftRightMargin, right: leftRightMargin),
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(height: xxxSmallerSpacing),
+                                Text(DatabaseUtil.getText('FirstName'),
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .xSmall
+                                        .copyWith(fontWeight: FontWeight.w600)),
+                                const SizedBox(height: xxxTinierSpacing),
+                                TextFieldWidget(
+                                    value: state.profileDetailsMap['fname'],
+                                    textInputAction: TextInputAction.next,
+                                    maxLength: 50,
+                                    hintText: DatabaseUtil.getText('FirstName'),
+                                    onTextFieldChanged: (String textField) {
+                                      copyProfileMap['fname'] = textField;
+                                    }),
+                                const SizedBox(height: xxTinySpacing),
+                                Text(DatabaseUtil.getText('LastName'),
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .xSmall
+                                        .copyWith(fontWeight: FontWeight.w600)),
+                                const SizedBox(height: xxxTinierSpacing),
+                                TextFieldWidget(
+                                    value: state.profileDetailsMap['lname'],
+                                    maxLength: 50,
+                                    textInputAction: TextInputAction.next,
+                                    hintText: DatabaseUtil.getText('LastName'),
+                                    onTextFieldChanged: (String textField) {
+                                      copyProfileMap['lname'] = textField;
+                                    }),
+                                const SizedBox(height: xxTinySpacing),
+                                Text(DatabaseUtil.getText('Contact'),
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .xSmall
+                                        .copyWith(fontWeight: FontWeight.w600)),
+                                const SizedBox(height: xxxTinierSpacing),
+                                TextFieldWidget(
+                                    value: state.profileDetailsMap['contact'],
+                                    textInputType: TextInputType.phone,
+                                    textInputAction: TextInputAction.done,
+                                    maxLength: 20,
+                                    hintText: DatabaseUtil.getText('Contact'),
+                                    onTextFieldChanged: (String textField) {
+                                      copyProfileMap['contact'] = textField;
+                                    }),
+                                const SizedBox(height: xxTinySpacing),
+                                Text(DatabaseUtil.getText('BloodGroup'),
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .xSmall
+                                        .copyWith(fontWeight: FontWeight.w600)),
+                                const SizedBox(height: xxxTinierSpacing),
+                                BloodGroupExpansionTile(
+                                    profileDetailsMap: copyProfileMap),
+                                const SizedBox(height: xxTinySpacing),
+                                SignaturePad(
+                                    map: copyProfileMap, mapKey: 'sign'),
+                                const SizedBox(height: xxxSmallerSpacing),
+                                PrimaryButton(
+                                    onPressed: () {
+                                      context.read<ProfileBloc>().add(
+                                          UpdateProfile(
+                                              updateProfileMap:
+                                                  copyProfileMap));
+                                    },
+                                    textValue:
+                                        DatabaseUtil.getText('buttonSave')),
+                                const SizedBox(height: xxxSmallerSpacing)
+                              ])));
+                } else if (state is EditProfileError) {
+                  return Center(
+                      child: GenericReloadButton(
+                          onPressed: () {
+                            context
+                                .read<ProfileBloc>()
+                                .add(DecryptUserProfileData());
+                          },
+                          textValue: StringConstants.kReload));
+                } else {
+                  return const SizedBox();
+                }
+              })),
+    );
   }
 }
