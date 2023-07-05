7801d6ab7684a4e10a2bd22aa809150c6dd7aa75
Rucha Wajpe <rucha.wajpe2233@gmail.com>
2023-07-05 19:32:44 +0530

    TXAD-111: Implement Pop-Up Menu to Incident Details Screen.

--- lib/blocs/incident/incidentDetails/incident_details_states.dart
+++ lib/blocs/incident/incidentDetails/incident_details_states.dart
@@ -1,10 +1,21 @@
+import 'package:equatable/equatable.dart';
+
 import '../../../data/models/incident/incident_details_model.dart';
 
-abstract class IncidentDetailsStates {}
+abstract class IncidentDetailsStates extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
-class IncidentDetailsInitial extends IncidentDetailsStates {}
+class IncidentDetailsInitial extends IncidentDetailsStates {
+  @override
+  List<Object?> get props => [];
+}
 
-class FetchingIncidentDetails extends IncidentDetailsStates {}
+class FetchingIncidentDetails extends IncidentDetailsStates {
+  @override
+  List<Object?> get props => [];
+}
 
 class IncidentDetailsFetched extends IncidentDetailsStates {
   final IncidentDetailsModel incidentDetailsModel;
@@ -12,6 +23,21 @@ class IncidentDetailsFetched extends IncidentDetailsStates {
 
   IncidentDetailsFetched(
       {required this.clientId, required this.incidentDetailsModel});
+
+  @override
+  List<Object?> get props => [];
 }
 
-class IncidentDetailsNotFetched extends IncidentDetailsStates {}
+class IncidentDetailsNotFetched extends IncidentDetailsStates {
+  @override
+  List<Object?> get props => [];
+}
+
+class IncidentDetailsPopUpMenuItemsFetched extends IncidentDetailsStates {
+  final List popUpMenuItems;
+
+  IncidentDetailsPopUpMenuItemsFetched({required this.popUpMenuItems});
+
+  @override
+  List<Object?> get props => [popUpMenuItems];
+}
---
+++ lib/screens/incident/incident_pop_up_menu_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:toolkit/configs/app_dimensions.dart';
+import 'package:toolkit/configs/app_theme.dart';
+import '../../../configs/app_spacing.dart';
+import '../../../utils/database_utils.dart';
+import '../../blocs/incident/incidentDetails/incident_details_bloc.dart';
+import '../../blocs/incident/incidentDetails/incident_details_event.dart';
+import '../../blocs/incident/incidentDetails/incident_details_states.dart';
+
+class IncidentDetailsPopUpMenu extends StatelessWidget {
+  const IncidentDetailsPopUpMenu({Key? key}) : super(key: key);
+
+  PopupMenuItem _buildPopupMenuItem(context, String title, String position) {
+    return PopupMenuItem(
+      value: position,
+      child: Text(title, style: Theme.of(context).textTheme.xSmall),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    context
+        .read<IncidentDetailsBloc>()
+        .add(IncidentDetailsFetchPopUpMenuItems(popUpMenuItems: const []));
+    return BlocBuilder<IncidentDetailsBloc, IncidentDetailsStates>(
+        buildWhen: (previousState, currentState) =>
+            currentState is IncidentDetailsPopUpMenuItemsFetched,
+        builder: (context, state) {
+          if (state is IncidentDetailsPopUpMenuItemsFetched) {
+            return PopupMenuButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(kCardRadius),
+                ),
+                iconSize: kIconSize,
+                icon: const Icon(Icons.more_vert_outlined),
+                offset: const Offset(0, xxTinierSpacing),
+                onSelected: (value) {
+                  if (value == DatabaseUtil.getText('AddComments')) {}
+                  if (value == DatabaseUtil.getText('EditIncident')) {}
+                  if (value == DatabaseUtil.getText('Report')) {}
+                  if (value == DatabaseUtil.getText('Markasresolved')) {}
+                  if (value == DatabaseUtil.getText('GenerateReport')) {}
+                },
+                position: PopupMenuPosition.under,
+                itemBuilder: (BuildContext context) {
+                  return List.generate(state.popUpMenuItems.length, (index) {
+                    return _buildPopupMenuItem(
+                        context,
+                        state.popUpMenuItems[index],
+                        state.popUpMenuItems[index]);
+                  });
+                });
+          } else {
+            return const SizedBox();
+          }
+        });
+  }
+}
